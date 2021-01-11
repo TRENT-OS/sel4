84445b347526926482b26141d48a951ae88a6bcc
Goran Badevic <goran.badevic@hensoldt-cyber.com>
2021-01-11 14:34:51 +0100

    Load seL4 into external RAM.

--- include/plat/spike/plat/64/plat_mode/machine/hardware.h
+++ include/plat/spike/plat/64/plat_mode/machine/hardware.h
@@ -27,7 +27,11 @@
 #define PPTR_BASE        0xFFFFFFC000000000lu
 /* This is the mapping of the kernel (mapped above the kernel window currently) */
 #define KERNEL_BASE      0xFFFFFFFF80000000lu
-#define KERNEL_ELF_BASE  0xFFFFFFFF81080000lu
+#define KERNEL_ELF_BASE  0xFFFFFFFF81000000lu
+/*
+#define KERNEL_BASE      0xFFFFFFFFc0000000lu
+#define KERNEL_ELF_BASE  0xFFFFFFFFc1000000lu
+*/
 #else
 #error Only PT_LEVELS == 3 is supported
 #endif
